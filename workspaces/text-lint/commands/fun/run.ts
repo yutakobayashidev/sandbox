5c92cdf757d9ffaa89d19e3e43ef79c304c4997e
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-10-17 18:32:00 +0900

    fix: prompt

--- workspaces/text-lint/commands/fun/run.ts
+++ workspaces/text-lint/commands/fun/run.ts
@@ -24,7 +24,12 @@ export const command = {
     const chatCompletion = await openai.chat.completions.create({
       messages: [
         { role: "system", content: SYSTEM_PROMPT },
-        { role: "user", content: interaction.options.getString("文字列") },
+        {
+          role: "user",
+          content: `敬語に変換するテキスト： ${interaction.options.getString(
+            "文字列"
+          )}`,
+        },
       ],
       model: "gpt-3.5-turbo",
     });
