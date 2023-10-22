d4461828dff9515fa21699994e05d4107b989f64
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-10-22 17:37:28 +0900

    feat: kotobade-asobou-web

---
+++ apps/kotobade-asobou-web/src/app/api/completion/route.ts
@@ -0,0 +1,103 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import {
+  CHINESE_SYSTEM_PROMPT,
+  GOJYUN_SYSTEM_PROMPT,
+  HEIAN_SYSTEM_PROMPT,
+  JAPANIIZU_SYSTEM_PROMPT,
+  KATAKANA_SYSTEM_PROMPT,
+  SYOGAKUSEI_SYSTEM_PROMPT,
+  WIRED_JAPANESE_SYSTEM_PROMPT,
+  KEIGO_SYSTEM_PROMPT,
+} from "kotobade-asobou";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+export const runtime = "edge";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY!,
+});
+
+const redis = new Redis({
+  token: process.env.UPSTASH_REDIS_REST_TOKEN || "",
+  url: process.env.UPSTASH_REDIS_REST_URL || "",
+});
+
+function get_type(type: string) {
+  switch (type) {
+    case "chinese":
+      return CHINESE_SYSTEM_PROMPT;
+    case "gojyun":
+      return GOJYUN_SYSTEM_PROMPT;
+    case "heian":
+      return HEIAN_SYSTEM_PROMPT;
+    case "japaniizu":
+      return JAPANIIZU_SYSTEM_PROMPT;
+    case "katakana":
+      return KATAKANA_SYSTEM_PROMPT;
+    case "keigo":
+      return KEIGO_SYSTEM_PROMPT;
+    case "syogakusei":
+      return SYOGAKUSEI_SYSTEM_PROMPT;
+    case "wired_japanese":
+      return WIRED_JAPANESE_SYSTEM_PROMPT;
+    default:
+      return null;
+  }
+}
+export async function POST(req: Request) {
+  if (
+    process.env.NODE_ENV != "development" &&
+    process.env.UPSTASH_REDIS_REST_URL &&
+    process.env.UPSTASH_REDIS_REST_TOKEN
+  ) {
+    const ip = req.headers.get("x-forwarded-for");
+
+    const ratelimit = new Ratelimit({
+      limiter: Ratelimit.slidingWindow(20, "1 d"),
+      redis: redis,
+    });
+
+    const { limit, remaining, reset, success } = await ratelimit.limit(
+      `capitalens_ratelimit_${ip}`
+    );
+
+    if (!success) {
+      return new Response("Too many requests", {
+        headers: {
+          "X-RateLimit-Limit": limit.toString(),
+          "X-RateLimit-Remaining": remaining.toString(),
+          "X-RateLimit-Reset": reset.toString(),
+        },
+        status: 429,
+      });
+    }
+  }
+
+  const { prompt, type } = await req.json();
+
+  const system_prompt = get_type(type);
+
+  if (!system_prompt) {
+    return new Response("問題が発生しました", {
+      status: 500,
+    });
+  }
+
+  const response = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: system_prompt },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    model: "gpt-4",
+    stream: true,
+  });
+
+  const stream = OpenAIStream(response);
+
+  return new StreamingTextResponse(stream);
+}
---
+++ apps/kotobade-asobou-web/src/app/page.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { useCompletion } from "ai/react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import TextareaAutosize from "react-textarea-autosize";
+import { toast } from "sonner";
+
+export default function Completion() {
+  const {
+    completion,
+    input,
+    stop,
+    isLoading,
+    handleInputChange,
+    handleSubmit,
+  } = useCompletion({
+    api: "/api/completion",
+    body: {
+      type: "chinese",
+    },
+    onResponse: (res) => {
+      if (res.status === 429) {
+        toast.error("制限されています。後でもう一度お試しください。");
+      } else if (res.status === 500) {
+        toast.error("問題が発生しました。");
+      }
+    },
+    onFinish: () => {
+      // do something with the completion result
+      toast.success("Successfully generated completion!");
+    },
+  });
+
+  return (
+    <div className="mx-auto py-12 space-y-3 max-w-3xl">
+      <h1 className="text-5xl mb-10 text-center font-bold">🌏 言葉で遊ぼう</h1>
+      <Select defaultValue="chinese">
+        <SelectTrigger className="w-full">
+          <SelectValue placeholder="選択してください" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="chinese">エセ中国語</SelectItem>
+          <SelectItem value="gojyun">変換します日本語の語順</SelectItem>
+          <SelectItem value="heian">平安時代</SelectItem>
+          <SelectItem value="japaniizu">Japaniizu</SelectItem>
+          <SelectItem value="katakana">カタカナ語</SelectItem>
+          <SelectItem value="keigo">敬語変換</SelectItem>
+          <SelectItem value="syogakusei">簡単な言葉で話す</SelectItem>
+          <SelectItem value="wired_japanese">怪しい日本語</SelectItem>
+        </SelectContent>
+      </Select>
+      <form onSubmit={handleSubmit}>
+        <TextareaAutosize
+          value={input}
+          className="w-full p-2 bg-gray-100"
+          placeholder="変換したい文章を入力..."
+          onChange={handleInputChange}
+        />
+        <div className="flex mt-3 items-center gap-3 justify-center">
+          <button
+            className="bg-red-500 font-bold px-6 py-2 text-white"
+            type="button"
+            onClick={stop}
+          >
+            ストップ
+          </button>
+          <button
+            className="bg-blue-500 font-bold px-6 py-2 text-white"
+            disabled={isLoading}
+            type="submit"
+          >
+            送信する
+          </button>
+        </div>
+        <div className="text-3xl mb-5 mt-8 block font-bold">
+          変換されたテキスト:
+        </div>
+        <div className="border rounded-md p-6">
+          {completion.length === 0
+            ? "送信ボタンをクリックしてください"
+            : completion}
+        </div>
+      </form>
+    </div>
+  );
+}
