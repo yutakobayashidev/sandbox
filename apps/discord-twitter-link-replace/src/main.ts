de86f72c5248734d4055f5caebdcadbf9f048c96
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-11-15 04:28:21 +0900

    done

--- apps/discord-twitter-link-replace/src/main.ts
+++ apps/discord-twitter-link-replace/src/main.ts
@@ -4,13 +4,33 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const client = new Client({
-  // See other options here
-  // https://discordjs-self-v13.netlify.app/#/docs/docs/main/typedef/ClientOptions
-  // All partials are loaded automatically
+  checkUpdate: false,
 });
 
 client.on("ready", async () => {
   if (client?.user) console.log(`${client.user.username} is ready!`);
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.id === client.user?.id && !message.system) {
+    let updatedContent = message.content;
+    updatedContent = updatedContent.replace(
+      /https:\/\/twitter\.com/g,
+      "https://fxtwitter.com"
+    );
+    updatedContent = updatedContent.replace(
+      /https:\/\/x\.com/g,
+      "https://fxtwitter.com"
+    );
+
+    if (message.content !== updatedContent) {
+      try {
+        await message.edit(updatedContent);
+      } catch (error) {
+        console.error("Failed to edit message:", error);
+      }
+    }
+  }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN as string);
