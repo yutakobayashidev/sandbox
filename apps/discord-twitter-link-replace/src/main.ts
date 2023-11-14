391ef0f3220d0f6bd1db472868589efc85cc1c12
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-11-15 04:35:13 +0900

    chore

--- apps/discord-twitter-link-replace/src/main.ts
+++ apps/discord-twitter-link-replace/src/main.ts
@@ -12,17 +12,35 @@ client.on("ready", async () => {
 });
 
 client.on("messageCreate", async (message) => {
+  // Check if the message is from the self bot and is not a system message
   if (message.author.id === client.user?.id && !message.system) {
     let updatedContent = message.content;
+
+    // Function to remove query parameters from URLs
+    const removeQueryParameters = (url) => {
+      const urlObject = new URL(url);
+      urlObject.search = ""; // Remove query parameters
+      return urlObject.toString();
+    };
+
+    // Replace twitter.com and x.com URLs and remove query parameters if present
     updatedContent = updatedContent.replace(
-      /https:\/\/twitter\.com/g,
-      "https://fxtwitter.com"
+      /https:\/\/twitter\.com\/\w+\/status\/\d+\S*/g,
+      (match) => {
+        return removeQueryParameters(match).replace(
+          "twitter.com",
+          "fxtwitter.com"
+        );
+      }
     );
     updatedContent = updatedContent.replace(
-      /https:\/\/x\.com/g,
-      "https://fxtwitter.com"
+      /https:\/\/x\.com\/\w+\/status\/\d+\S*/g,
+      (match) => {
+        return removeQueryParameters(match).replace("x.com", "fxtwitter.com");
+      }
     );
 
+    // Edit the message if the content has changed
     if (message.content !== updatedContent) {
       try {
         await message.edit(updatedContent);
