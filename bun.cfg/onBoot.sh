426ad1d5e729b46ae3dff668b9d94ff067e9e647
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-11-17 03:11:45 +0900

    del

--- bun.cfg/onBoot.sh
+++
@@ -1,8 +0,0 @@
-if [ -d "../.bun/bin" ]; then
-  echo "Bun is installed!"
-  clear
-else 
-  echo "Installing bun..."
-  curl https://bun.sh/install | bash
-  clear
-fi
--- bun.cfg/preload.sh
+++
@@ -1,7 +0,0 @@
-clear
-if [ -d "../.bun/bin" ]; then
-  clear
-else 
-  curl https://bun.sh/install | bash
-  clear
-fi
