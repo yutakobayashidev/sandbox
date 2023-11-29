dbe43fd13581f8e1213e1e2ded5e32bd1df826d9
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-11-29 15:26:08 +0900

    Add WorkOS authentication and update dependencies

---
+++ apps/authkit/src/app/callback/route.ts
@@ -0,0 +1,23 @@
+import { WorkOS } from "@workos-inc/node";
+import { redirect } from "next/navigation";
+
+const workos = new WorkOS(process.env.WORKOS_API_KEY);
+
+export async function GET(request: Request) {
+  const code = new URL(request.url).searchParams.get("code") || "";
+
+  let response;
+
+  try {
+    response = await workos.userManagement.authenticateWithCode({
+      clientId: process.env.WORKOS_CLIENT_ID || "",
+      code,
+    });
+  } catch (error) {
+    response = error;
+  }
+
+  if (response) {
+    redirect(`http://localhost:3000/?response=${JSON.stringify(response)}`);
+  }
+}
